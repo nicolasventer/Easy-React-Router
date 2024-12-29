41a616b78f834b7225f5d181a64e84dcf21240f5
Nicolas VENTER <thedeal78@hotmail.fr>
2024-12-29 10:36:32 +0100

    fix post navigation

---
+++ docs/routes/index.chunk-4pt3s8cj.js
@@ -0,0 +1,5 @@
+import{u as j}from"../index.chunk-0tk2x9qp.js";import{w as k,y as q}from"../index.chunk-3gg1x40h.js";var b=j(()=>import("./index.lazy.chunk-cnncbsmj.js")),w=b.load,A=b.loadingState,B=b.getComponent("MainLayout");export{A as loadingState,w as load,B as MainLayout};
+export{B as a};
+
+//# debugId=4CCA449EC314851F64756E2164756E21
+//# sourceMappingURL=index.chunk-4pt3s8cj.js.map
--- src/routes/posts.tsx
+++ src/routes/posts.tsx
@@ -9,23 +9,24 @@ export const PostsLayout = () => (
 				<RouteLink path="/posts" style={{ color: currentRoute.value === "/posts" ? "blue" : "black", marginBottom: 10 }}>
 					Unselected
 				</RouteLink>
-				{Array.from({ length: 10 }).map((_, i) => (
-					<RouteLink
-						key={i}
-						path="/posts/:id"
-						params={{ id: i.toString() }}
-						style={{
-							color:
-								getRouteParams("/posts/:id").value.id === i.toString() || (i === 3 && currentRoute.value === "/posts/3")
-									? "blue"
-									: "black",
-						}}
-					>
-						Post {i}
-						{i === 3 ? " (special)" : ""}
-						{B_PROD && i < 5 ? " (statically deployed)" : ""}
-					</RouteLink>
-				))}
+				{Array.from({ length: 10 }).map((_, i) =>
+					i === 3 ? (
+						<RouteLink key={i} path="/posts/3" style={{ color: currentRoute.value === "/posts/3" ? "blue" : "black" }}>
+							Post {i} (special)
+							{B_PROD ? " (statically deployed)" : ""}
+						</RouteLink>
+					) : (
+						<RouteLink
+							key={i}
+							path="/posts/:id"
+							params={{ id: i.toString() }}
+							style={{ color: getRouteParams("/posts/:id").value.id === i.toString() ? "blue" : "black" }}
+						>
+							Post {i}
+							{B_PROD && i < 5 ? " (statically deployed)" : ""}
+						</RouteLink>
+					)
+				)}
 				<a href={`${clientEnv.BASE_URL}/posts/0/invalid`}>Invalid post{B_PROD ? " (statically deployed)" : ""}</a>
 				<a href={`${clientEnv.BASE_URL}/posts/1/invalid`}>Invalid post</a>
 			</div>
