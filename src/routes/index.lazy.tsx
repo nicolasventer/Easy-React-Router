07004a9c954d418fa118686ec6329067d1f2a38b
Nicolas VENTER <thedeal78@hotmail.fr>
2025-04-19 10:05:39 +0200

    NavigationItemParams

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -12,19 +12,23 @@ import {
 
 type NavigationItem<T extends RouterPathType> = { title: string; path: T; params: RouterParamsType<T> };
 
-const navigationItem = <T extends RouterPathType>(title: string, path: T, params: RouterParamsType<T>): NavigationItem<T> => ({
-	title,
-	path,
-	params,
+type NavigationItemParams<T extends RouterPathType> = keyof RouterParamsType<T> extends never
+	? [title: string, path: T]
+	: [title: string, path: T, params: RouterParamsType<T>];
+
+const navigationItem = <T extends RouterPathType>(...params: NavigationItemParams<T>): NavigationItem<T> => ({
+	title: params[0],
+	path: params[1],
+	params: params[2] as RouterParamsType<T>,
 });
 
 const navigationItems = [
-	navigationItem("Home", "/", {}),
+	navigationItem("Home", "/"),
 	navigationItem("Home with id", "?id", { id: "abc" }),
-	navigationItem("Posts", "/posts", {}),
-	navigationItem("About", "/about", {}),
-	navigationItem("HugePage", "/hugePage", {}),
-	navigationItem("InvalidPage", "/invalid" as "/", {}),
+	navigationItem("Posts", "/posts"),
+	navigationItem("About", "/about"),
+	navigationItem("HugePage", "/hugePage"),
+	navigationItem("InvalidPage", "/invalid" as "/"),
 ] as const;
 
 // @routeExport
